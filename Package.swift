4d82a16881c5e66ac9830886093e880907d55c0e
Tom Smallwood <tom@agilerocket.com>
2018-04-25 09:41:25 -0700

    Added ability to create and edit acronyms

--- Package.swift
+++ Package.swift
@@ -10,7 +10,8 @@ let package = Package(
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
 
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2")
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc.3.1")
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor", "FluentPostgreSQL", "Leaf"]),
--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -9,6 +9,11 @@ struct WebsiteController: RouteCollection {
         router.get("users", use: allUsersHandler)
         router.get("categories", Category.parameter, use: categoryHandler)
         router.get("categories", use: allCategoriesHandler)
+        router.get("create-acronym", use: createAcronymHandler)
+        router.post("create-acronym", use: createAcronymPostHandler)
+        router.get("acronyms", Acronym.parameter, "edit", use: editAcronymHandler)
+        router.post("acronyms", Acronym.parameter, "edit", use: editAcronymPostHandler)
+        router.post("acronyms", Acronym.parameter, "delete", use: deleteAcronymHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
@@ -66,6 +71,45 @@ struct WebsiteController: RouteCollection {
         }
     }
     
+    func createAcronymPostHandler(_ req: Request) throws -> Future<Response> {
+        return try req.content.decode(AcronymPostData.self).flatMap(to: Response.self) { data in
+            let acronym = Acronym(short: data.acronymShort, long: data.acronymLong, creatorID: data.creator)
+            return acronym.save(on: req).map(to: Response.self) { acronym in
+                guard let id = acronym.id else {
+                    return req.redirect(to: "/")
+                }
+                return req.redirect(to: "/acronyms/\(id)")
+            }
+        }
+    }
+    
+    func editAcronymHandler(_ req: Request) throws -> Future<View> {
+        return try flatMap(to: View.self, req.parameters.next(Acronym.self), User.query(on: req).all()) { acronym, users in
+            let context = EditAcronymContext(title: "Edit Acronym", acronym: acronym, users: users)
+            return try req.leaf().render("createAcronym", context)
+        }
+    }
+    
+    func editAcronymPostHandler(_ req: Request) throws -> Future<Response> {
+        return try flatMap(to: Response.self, req.parameters.next(Acronym.self), req.content.decode(AcronymPostData.self)) { acronym, data in
+            acronym.short = data.acronymShort
+            acronym.long = data.acronymLong
+            acronym.creatorID = data.creator
+            
+            return acronym.save(on: req).map(to: Response.self) { acronym in
+                guard let id = acronym.id else {
+                    return req.redirect(to: "/")
+                }
+                return req.redirect(to: "/acronyms/\(id)")
+            }
+        }
+    }
+    
+    func deleteAcronymHandler(_ req: Request) throws -> Future<Response> {
+        return try req.parameters.next(Acronym.self).flatMap(to: Response.self) { acronym in
+            return acronym.delete(on: req).transform(to: req.redirect(to: "/"))
+        }
+    }
 }
 
 extension Request {
@@ -111,4 +155,18 @@ struct AllCategoriesContext: Encodable {
 struct CreateAcronymContext: Encodable {
     let title: String
     let users: [User]
+}
+
+struct AcronymPostData: Content {
+    static var defaultMediaType = MediaType.urlEncodedForm
+    let acronymLong: String
+    let acronymShort: String
+    let creator: UUID
+}
+
+struct EditAcronymContext: Encodable {
+    let title: String
+    let acronym: Acronym
+    let users: [User]
+    let editing = true
 }
